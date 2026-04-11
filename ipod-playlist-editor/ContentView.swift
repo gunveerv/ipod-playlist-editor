e0439b16e8640bc4ae4b14a5695d1ef78bc81687
Gunveer Vilkhu <gunveerv123@gmail.com>
2026-04-10 21:17:20 -0400

    add a button and path finder

--- ipod-playlist-editor/ContentView.swift
+++ ipod-playlist-editor/ContentView.swift
@@ -8,12 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isShowing = false
+    @State var pathToPlayer = ""
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Path to iPod")
+            Button("test") {
+                isShowing = true
+            }
+            .fileImporter(isPresented: $isShowing, allowedContentTypes: [.folder]) { result in
+                
+                switch result {
+                    
+                case .success(let path):
+                    // handle success path
+                    print(path)
+                    
+                case .failure(let error):
+                    // handle error
+                    print(error)
+                }
+            }
         }
         .padding()
     }
